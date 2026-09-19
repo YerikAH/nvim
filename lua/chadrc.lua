3e0096f9c5853f6229584a5bc94e1dff83682188
Harvey <yerikah@users.noreply.github.com>
2026-09-18 19:01:25 -0500

    round tabufline buffers

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -15,6 +15,41 @@ M.base46 = {
 	-- },
 }
 
+local function rounded_buffers()
+	local api = vim.api
+	local style_buf = require("nvchad.tabufline.utils").style_buf
+	local buffers = {}
+
+	vim.t.bufs = vim.tbl_filter(api.nvim_buf_is_valid, vim.t.bufs or {})
+
+	for i, nr in ipairs(vim.t.bufs) do
+		local active = api.nvim_get_current_buf() == nr
+		local group = active and "TbBufOn" or "TbBufOff"
+		local hl = api.nvim_get_hl(0, { name = group, link = false })
+		local separator = "TbRound" .. (active and "On" or "Off")
+
+		api.nvim_set_hl(0, separator, {
+			fg = hl.bg or hl.fg,
+			bg = "NONE",
+		})
+
+		table.insert(buffers, "%#" .. separator .. "#" .. style_buf(nr, i, 21) .. "%#" .. separator .. "#")
+	end
+
+	return table.concat(buffers) .. "%="
+end
+
+M.ui = {
+	statusline = {
+		separator_style = "round",
+	},
+	tabufline = {
+		modules = {
+			buffers = rounded_buffers,
+		},
+	},
+}
+
 -- M.nvdash = { load_on_startup = true }
 -- M.ui = {
 --       tabufline = {
